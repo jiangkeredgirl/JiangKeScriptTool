75ecad0c56c754b44f50d68fe8127e63776ee802
jiangke <jiangkeredgirl@hotmail.com>
2024-04-28 18:11:52 +0800

    更新buildevent的复制dll脚本

--- bincopy.bat
+++ bincopy.bat
@@ -1,18 +1,20 @@
 set dest_path=%1
+set build_type=%2
 cd /d %~dp0
 
 if "%dest_path%"=="" ( echo dest_path is null) else (
-echo dest_path is "%dest_path%
+echo dest_path is %dest_path%
+echo build_type is %build_type%
 
-echo f|xcopy /y /s /i /f .\libs\CCAppCommon\Debug\*                            %dest_path%\
-echo f|xcopy /y /s /i /f .\libs\CCFbo\Debug\*                                  %dest_path%\
-echo f|xcopy /y /s /i /f .\libs\CCPluginAPI\Debug\*                            %dest_path%\
-echo f|xcopy /y /s /i /f .\libs\qCC_db\Debug\*                                 %dest_path%\
-echo f|xcopy /y /s /i /f .\libs\qCC_db\extern\CCCoreLib\Debug\*                %dest_path%\
-echo f|xcopy /y /s /i /f .\libs\qCC_glWindow\Debug\*                           %dest_path%\
-echo f|xcopy /y /s /i /f .\libs\qCC_io\Debug\*                                 %dest_path%\
-echo f|xcopy /y /s /i /f .\plugins\core\Standard\qMeasure\Debug\*              %dest_path%\plugins\
+echo f|xcopy /y /s /i /f ..\..\libs\CCAppCommon\%build_type%\*                            %dest_path%\
+echo f|xcopy /y /s /i /f ..\..\libs\CCFbo\%build_type%\*                                  %dest_path%\
+echo f|xcopy /y /s /i /f ..\..\libs\CCPluginAPI\%build_type%\*                            %dest_path%\
+echo f|xcopy /y /s /i /f ..\..\libs\qCC_db\%build_type%\*                                 %dest_path%\
+echo f|xcopy /y /s /i /f ..\..\libs\qCC_db\extern\CCCoreLib\%build_type%\*                %dest_path%\
+echo f|xcopy /y /s /i /f ..\..\libs\qCC_glWindow\%build_type%\*                           %dest_path%\
+echo f|xcopy /y /s /i /f ..\..\libs\qCC_io\%build_type%\*                                 %dest_path%\
+echo f|xcopy /y /s /i /f ..\..\plugins\core\Standard\qMeasure\%build_type%\*              %dest_path%\plugins\
 
 )
 
-rem pause 
+rem pause
--- bincopyrun.bat
+++ bincopyrun.bat
@@ -1,5 +1,7 @@
+set build_type=%1
+echo build_type is %build_type%
 cd /d %~dp0
 echo "cc bin files copy"
-start bincopy.bat ".\qCC\Debug"
+start bincopy.bat "..\%build_type%" %build_type%
 
-rem pause 
+rem pause
